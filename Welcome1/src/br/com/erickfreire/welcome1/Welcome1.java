855fb183340f4cac25719c3408f9492edeffbe70
Erick Freire <erilefr@gmail.com>
2022-09-30 23:00:13 -0300

    Version1
    Version1

---
+++ Welcome1/src/br/com/erickfreire/welcome1/Welcome1.java
@@ -0,0 +1,16 @@
+package br.com.erickfreire.welcome1;
+
+/**
+ * Welcome to Java Programming!
+ * @author Erick Freire
+ * @version 1 - Created in 09-30-2022
+ */
+
+public class Welcome1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Programming!");
+
+	}
+
+}
